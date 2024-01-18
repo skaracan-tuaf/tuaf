e5aa08326f34e2411e835e5740ce0a618a5b0887
skaracan-tuaf <111881760+skaracan-tuaf@users.noreply.github.com>
2024-01-18 16:55:49 +0300

    Update analiz.js

--- analiz.js
+++ analiz.js
@@ -1,3 +1,29 @@
+// 
+* Satıştan sonra stok azalt. Sıfır olanları gösterme veya Stokta yok etiketi yap.
+* Ürün Ekle sayfasındaki combobox'taki gibi diğerlerini düzelt. 
+    Kategori Ekle/Düzenle, SSS, Ayarlar
+* Kategoriler index'inde toplam satış ve toplam kazanç sütunu oluştur
+* Öznitelikleri kapat
+* Kupon oluştur
+* Müşteri ekle
+* İstatistikleri kapat
+* Profil sayfası ile veritabanını bağla
+* Ziyaretçi trafiğini yap
+* Sosyal medyaları ekle
+* Müşteriler adres ekleyip güncellesin
+* Eğer müşteri oturum açmışsa ürüne yorum eklerken sadece yorum bölümü görünsün
+* Eğer müşteri oturum açmışsa ödeme bölümünde müşterinin adresi otomatik gelsin
+* Gizlilik Sözleşmesi, Mesafeli Satış Sözleşmesi, İade Sözleşmesi, Üyelik Sözleşmesi,
+* Üye Aydınlatma Metni, Ticari Elektronik İleti İzni gibi sözleşle ekleme sayfası yap.
+* Sık Sorulan Sorular sayfasında Üst Başlık kısmından seçin yapılmışsa Başlık kısmını kapat.
+* İletişim sayfası statik yapılabilir.
+* Favoriye ekleme olayını çerezler ile yap.
+* Sipariş Takip Sayfası yap. 
+* Arama olayını kodla.
+* Resim eklerken crop yapılabilsin.
+* Bildirim butonunu kodla. Arama butonunu kapat.
+//
+    
 @php
     $monthlySales = DB::table('order_items')
         ->join('orders', 'order_items.order_id', '=', 'orders.id')
